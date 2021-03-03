544c1dedfc530e3ea7d83d58ab8fcf85bf473045
Roberts Jumis <roberts.jumis@intelligentsystems.lv>
2021-03-03 17:22:58 +0200

    Fetch data using SOAP & quick filtering implemented. Needs refactor

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,13 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SoapClient;
 
 class CompanyController extends Controller
 {
-    public function show()
+    public function show(Request $request)
     {
-        return response()->json([
-            'status' => '200'
-        ], 200);
+        $filter = $request->input('filter');
+        $soapClient = new SoapClient("http://test.soa.energo.lv:8011/DMBServices/Private/DMBService?wsdl",
+            [
+                'user' => 'portal_user',
+                'password' => 'yon4I1TP'
+            ]);
+        $list = $soapClient->GetSuppliersList([
+            'MMIdentification' => [
+                'username' => '*DAMBO*',
+                'password' => 'u4U3hHiKa5ky4SDw',
+                'supplierEIC' => 'mcnffDxWh5NEZ6BwkAvUNtsVdwaUSUeb',
+                'messageId' => 'string'
+            ]
+        ]);
+        $list = json_decode(json_encode($list), true);
+        $filtered = [];
+
+        foreach ($list['suppliersList']['SuppliersEntry'] as $entry)
+        {
+            if (((stripos($entry['supplierName'], $filter) !== false) || (stripos($entry['registrationNumber'], $filter) !== false)) && $entry['isActive'] == 'Y') {
+                $filtered[] = $entry;
+            }
+        }
+
+
+
+
+        return response()->json($filtered, 200);
     }
 }
